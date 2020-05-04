1056ae5255a40d3bdb75151a50e5ee38f8f487f5
Sylwia Rusek <sylwia02.02.1999@interia.pl>
2020-05-04 16:28:20 +0200

    Uzupelnienie klasy PojemnikProduktow

--- system_Automatu.py
+++ system_Automatu.py
@@ -37,3 +37,54 @@ class PojemnikMonet:
             for wart,ilosc in monetyDoWydania.items():
                 self.Monety[wart]+=ilosc
                 return False
+
+class PojemnikProduktow:
+    ListaProduktow={"30":{"nazwa":"woda gazowana","cena":351,"ilosc":5},
+                    "31":{"nazwa":"woda niegazowana","cena":351,"ilosc":5},
+                    "32":{"nazwa":"sok pomarańczowy","cena":406,"ilosc":5},
+                    "33":{"nazwa":"sok porzeczkowy","cena":406,"ilosc":5},
+                    "34":{"nazwa":"sok jabłkowy","cena":406,"ilosc":5},
+                    "35":{"nazwa":"mirinda","cena":450,"ilosc":5},
+                    "36":{"nazwa":"pepsi","cena":450,"ilosc":0},
+                    "37":{"nazwa":"pepsi light","cena":460,"ilosc":5},
+                    "38":{"nazwa":"cola","cena":380,"ilosc":5},
+                    "39":{"nazwa":"cola zero","cena":390,"ilosc":5},
+                    "40":{"nazwa":"nestea brzoskwiniowe","cena":469,"ilosc":5},
+                    "41":{"nazwa":"nestea cytrynowe","cena":469,"ilosc":5},
+                    "42":{"nazwa":"nestea zielona herbata","cena":469,"ilosc":5},
+                    "43":{"nazwa":"tymbark jakbło-mięta","cena":180,"ilosc":5},
+                    "44":{"nazwa":"tymbark pomarańcza-brzoskwinia","cena":180,"ilosc":5},
+                    "45":{"nazwa":"tymbark jabłko-wiśnia","cena":180,"ilosc":5},
+                    "46":{"nazwa":"tymbark jabłko-kiwi","cena":180,"ilosc":5},
+                    "47":{"nazwa":"sprite","cena":395,"ilosc":5},
+                    "48":{"nazwa":"7up","cena":400,"ilosc":5},
+                    "49":{"nazwa":"oranżada cytrynowa","cena":240,"ilosc":5},
+                    "50":{"nazwa":"oranżada wiśniowa","cena":240,"ilosc":5},
+    }
+    def __init__(self):
+        pass
+
+    def czyjest(self,numer):
+        if numer in self.ListaProduktow.keys() and self.ListaProduktow[numer]["ilosc"]>0:
+            return True
+        else:
+            return False
+
+    def cena(self, numer):
+        if numer in self.ListaProduktow:
+            return self.ListaProduktow[numer]["cena"]
+        else:
+            return False
+
+    def nazwa(self,numer):
+        if numer in self.ListaProduktow:
+            return self.ListaProduktow[numer]["nazwa"]
+        else:
+            return False
+
+    def zmniejsziwypisz(self,numer):
+        if numer in self.ListaProduktow and self.ListaProduktow[numer]["ilosc"]>0:
+            self.ListaProduktow[numer]["ilosc"]-=1
+            return self.ListaProduktow[numer]
+        return False
+

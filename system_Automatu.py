35dd9f3ac59369a05ddedd6a9313dc3f242323fd
Sylwia Rusek <sylwia02.02.1999@interia.pl>
2020-05-04 13:48:11 +0200

    Uzupelnienie klasy PojemnikMonet

--- system_Automatu.py
+++ system_Automatu.py
@@ -14,3 +14,26 @@ class PojemnikMonet:
     def dodajMonety(self,monety):
         for wartosc,ilosc in monety.items():
             self.Monety[wartosc]+=ilosc
+
+    def zwrocWartosc(self,wartoscDoWydania):
+        wartosciMonet=[500,200,100,50,20,10,5,2,1]
+        monetyDoWydania={}
+        for w in wartosciMonet:
+            monetyDoWydania[w]=0
+        while wartoscDoWydania>0:
+            wartMonety=0
+            for x in wartosciMonet:
+                if x <= wartoscDoWydania and self.Monety[x] >0:
+                    wartMonety=x
+                    wartoscDoWydania-=x
+                    self.Monety[x]-=1
+                    break
+            if wartMonety==0:
+                break
+            monetyDoWydania[wartMonety]+=1
+        if wartoscDoWydania==0:
+            return monetyDoWydania
+        else:
+            for wart,ilosc in monetyDoWydania.items():
+                self.Monety[wart]+=ilosc
+                return False

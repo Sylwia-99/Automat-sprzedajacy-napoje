4c4ca21f1ba140db31f3b0c4e2af368f5b272e2c
Sylwia Rusek <sylwia02.02.1999@interia.pl>
2020-05-05 17:18:25 +0200

    Uzupelnienie klasy PojemnikMonet i wykonanie testow

--- system_Automatu.py
+++ system_Automatu.py
@@ -1,3 +1,4 @@
+from enum import Enum
 class PojemnikMonet:
     Monety={1:70,
             2:80,
@@ -111,3 +112,189 @@ class WyborKodu:
     def clear(self):
         self.kod = ""
 
+class Stan(Enum):
+    wyborProduktu=1
+    wprowadzanieMonet=2
+
+
+class Automat_z_napojami:
+    def __init__(self):
+        self.pojemnikMonet = PojemnikMonet()
+        self.pojemnikProduktow = PojemnikProduktow()
+        self.wyborKodu = WyborKodu()
+
+    funZwrocReszte=None
+    funZmianaKodu=None
+    funCzekajNaPieniadze=None
+    funWydajProdukt=None
+    funBlad=None
+    funNiemaWAautomacie=None
+    funTylkoOdliczonaKwota=None
+    funInformacje=None
+
+    PojemnikMonet=None
+    PojemnikProduktow=None
+    WyborKodu=None
+    Stan=Stan.wyborProduktu
+
+    MonetyWSchowku={
+        1:0,
+        2:0,
+        5:0,
+        10:0,
+        20:0,
+        50:0,
+        100:0,
+        200:0,
+        500:0
+    }
+    aktualnaKwotaWSchowku=0
+    wybranyProdukt=""
+
+    def wybierz(self,cyfra):
+        kod=self.wyborKodu.wybierz(cyfra)
+        if kod !=False:
+            if self.pojemnikProduktow.czyjest(kod):
+                self.wybranyProdukt=kod
+                self.Stan = Stan.wprowadzanieMonet
+                self.funZmianaKodu(kod)
+                self.funCzekajNaPieniadze(self.pojemnikProduktow.cena(kod))
+            else:
+                self.funNiemaWAautomacie(kod)
+        else:
+            self.funZmianaKodu(self.wyborKodu.stan())
+
+    def wrzucMonete(self,moneta):
+        if self.Stan == Stan.wprowadzanieMonet:
+            self.MonetyWSchowku[moneta]+=1
+            self.aktualnaKwotaWSchowku+=moneta
+            if self.aktualnaKwotaWSchowku>=self.pojemnikProduktow.cena(self.wybranyProdukt):
+                roznica=self.aktualnaKwotaWSchowku-self.pojemnikProduktow.cena(self.wybranyProdukt)
+                reszta=self.pojemnikMonet.zwrocWartosc(roznica)
+                if reszta==False:
+                    self.funZwrocReszte(self.MonetyWSchowku)
+                    self.funTylkoOdliczonaKwota()
+                else:
+                    self.pojemnikMonet.dodajMonety(self.MonetyWSchowku)
+                    self.funZwrocReszte(reszta)
+                    produkt=self.pojemnikProduktow.zmniejsziwypisz(self.wybranyProdukt)
+                    self.funWydajProdukt(produkt["nazwa"])
+                for monety in self.MonetyWSchowku:
+                    self.MonetyWSchowku[monety]=0
+                self.aktualnaKwotaWSchowku = 0
+                self.Stan =Stan.wyborProduktu
+        else:
+            self.funZwrocReszte({moneta: 1})
+
+    def przerwij(self):
+        if self.Stan==Stan.wprowadzanieMonet:
+            self.funZwrocReszte(self.MonetyWSchowku)
+            for monety in self.MonetyWSchowku:
+                self.MonetyWSchowku[monety]=0
+            self.aktualnaKwotaWSchowku=0
+            self.Stan=Stan.wyborProduktu
+
+    def onZwrocReszte(self,funZwrocReszte):
+        self.funZwrocReszte=funZwrocReszte
+
+    def onZmianaKodu(self,funZmianaKodu):
+        self.funZmianaKodu=funZmianaKodu
+
+    def onCzekajNaPieniadze(self,funCzekajNaPieniadze):
+        self.funCzekajNaPieniadze=funCzekajNaPieniadze
+
+    def onWydajProdukt(self,funWydajProdukt):
+        self.funWydajProdukt=funWydajProdukt
+
+    def onBlad(self,funBlad):
+        self.funBlad=funBlad
+
+    def onNiemaWAautomacie(self,funNiemaWAautomacie):
+        self.funNiemaWAautomacie=funNiemaWAautomacie
+
+    def onTylkoOdliczonaKwota(self,funTylkoOdliczonaKwota):
+        self.funTylkoOdliczonaKwota=funTylkoOdliczonaKwota
+
+    def onInformacje(self,funInformacje):
+        self.onInformacje=funInformacje
+
+
+if __name__=="__main__":
+
+    automat=Automat_z_napojami()
+    automat.onZwrocReszte(lambda monety: print("Wydano reszte:",monety))
+    automat.onZmianaKodu( lambda kod: print("Aktualny kod:", kod) )
+    automat.onCzekajNaPieniadze( lambda cena: print("Wrzuc pieniadze, cena produktu=", cena/100))
+    automat.onWydajProdukt( lambda produkt: print("Wydano produkt:", produkt) )
+    automat.onNiemaWAautomacie(lambda kod: print("W automacie nie ma produktu z numerem",kod))
+    automat.onBlad( lambda info: print(info) )
+
+    if automat.pojemnikProduktow.czyjest("36") ==False:
+        print("w Automacie nie ma produktu 36")
+    else:
+        print("w Automacie jest produkt 36")
+
+    automat.wrzucMonete(50)  # Wydano reszte: {50: 1}
+###Test 1
+    print("Cena produktu nr 36 wynosi",automat.pojemnikProduktow.cena("36")/100,"zl")
+
+###Test 2
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  #Wrzuc pieniadze, cena produktu=1.8
+    automat.wrzucMonete(100)
+    automat.wrzucMonete(50)
+    automat.wrzucMonete(20)
+    automat.wrzucMonete(10) #{500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0}#Wydano produkt: tymbark jabłko-kiwi
+###Test 3
+    automat.wybierz("3") # Aktualny kod: 3
+    automat.wybierz("5") # Aktualny kod: 35 # Wrzuc pieniadze, cena produktu= 4.5
+    automat.wrzucMonete(200)
+    automat.wrzucMonete(200)
+    automat.wrzucMonete(200) # Wydano reszte: {500: 0, 200: 0, 100: 1, 50: 1, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0} # Wydano produkt: pepsi
+###Test 4
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  # Aktualny kod: 46 #Wrzuc pieniadze,cena produktu= 1.8
+    automat.wrzucMonete(100)
+    automat.wrzucMonete(50)
+    automat.wrzucMonete(20)
+    automat.wrzucMonete(10)# {500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0} #Wydano produkt: tymbark jabłko-kiwi
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  # Aktualny kod:46 #Wrzuc pieniadze, cena produktu= 1.8
+    automat.wrzucMonete(100)
+    automat.wrzucMonete(50)
+    automat.wrzucMonete(20)
+    automat.wrzucMonete(10)# {500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0} #Wydano produkt: tymbark jabłko-kiwi
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  # Aktualny kod: 46 #Wrzuc pieniadze,cena produktu= 1.8
+    automat.wrzucMonete(100)
+    automat.wrzucMonete(50)
+    automat.wrzucMonete(20)
+    automat.wrzucMonete(10)# {500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0} #Wydano produkt: tymbark jabłko-kiwi
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  # Aktualny kod: 46 #Wrzuc pieniadze, cena produktu= 1.8
+    automat.wrzucMonete(100)
+    automat.wrzucMonete(50)
+    automat.wrzucMonete(20)
+    automat.wrzucMonete(10) # {500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0} #Wydano produkt: tymbark jabłko-kiwi
+    automat.wybierz("4")  # Aktualny kod: 4
+    automat.wybierz("6")  #W automacie nie ma produktu z numerem 46
+###Test 5
+    automat.wybierz("9") # Aktualny kod: 9
+    automat.wybierz("9") #W automacie nie ma produktu z numerem 99
+    automat.wybierz("1") # Aktualny kod: 1
+    automat.wybierz("9") # W automacie nie ma produktu z numerem 19
+###Test 6
+    automat.wybierz("3")  # Aktualny kod: 3
+    automat.wybierz("5")  # Aktualny kod: 35 # Wrzuc pieniadze, cena produktu= 4.5
+    automat.wrzucMonete(200)
+    automat.wrzucMonete(200)
+    automat.przerwij()#Wydano reszte: {1: 0, 2: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0, 200: 2, 500: 0}
+###Test 7
+
+
+###Test 8
+    automat.wybierz("4") # Aktualny kod: 4
+    automat.wybierz("5") # Aktualny kod: 45 # Wrzuc pieniadz, cena produktu= 1.8
+    for i in range(0,180):
+        automat.wrzucMonete(1) #Wydano reszte: {500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0}
+                                #Wydano produkt: tymbark jabłko-wiśnia
